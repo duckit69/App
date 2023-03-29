910d2378df89317fe8ad8622bfc058c7a37ff703
duckit69 <gherab.radouane@gmail.com>
2023-03-29 04:45:48 +0100

    Added WebSocket Logic

--- app.js
+++ app.js
@@ -5,9 +5,51 @@ const session = require('express-session');
 const userRoute = require('./routes/User/userRoute');
 const treatmentRoute = require('./routes/Treatment/treatmentRoute');
 const ejsMate = require('ejs-mate');
-const http = require('http');
-const app = express();
 
+const app = express();
+// WEB SOCKET RElATED STUFF
+const http = require('http');
+const WebSocket = require('ws');
+const serverApp = http.createServer(app);
+const s = new WebSocket.Server({ server: serverApp, path: '/chat' });
+
+const patients = new Map(); // Map of patient IDs to their sockets
+const doctors = new Map(); // Map of doctor IDs to their sockets
+
+s.on('connection', (socket, req) => {
+
+    console.log('WebSocket Connected');
+
+    socket.on('message', (message) => {
+        try {
+            const data = JSON.parse(message);
+            const { type, sender_id, receiver_id, content, role } = data;
+            if (type === 'login') {
+                if (role === 'patient') {
+                    patients.set(sender_id, socket);
+                } else {
+                    doctors.set(sender_id, socket);
+                }
+            } else if (type === 'message') {
+                const receiverSocket = role === 'patient' ? doctors.get(receiver_id) : patients.get(receiver_id)
+                if (receiverSocket) {
+                    const msg = JSON.stringify({type, sender_id, receiver_id, content})
+                    receiverSocket.send(msg);
+                }
+            }
+        } catch (e) {
+            console.log(err);
+        }
+    });
+
+    socket.on('close', () => {
+        console.log('WebSocket closed');
+    });
+})
+serverApp.listen(8080, () => {
+    console.log('Server Socket Listening on port 8080');
+})
+//
 app.set('view engine', 'ejs');
 app.engine('ejs', ejsMate);
 app.set('views', path.join(__dirname, 'views'));
